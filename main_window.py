06277e5a67037b24200b8057a354508f153d8b32
Khalil37 <khabdeddaim@gmail.com>
2023-08-19 19:14:43 +0100

    2st

--- main_window.py
+++ main_window.py
@@ -10,6 +10,11 @@ class MainWindow(QMainWindow):
         file_menu=menu.addMenu("File")
         quit_action=file_menu.addAction("Quit")
         quit_action.triggered.connect(self.quit)
+        edit_menu=menu.addMenu("Edit")
+        edit_menu.addAction("Copy")
+        edit_menu.addAction("Copy")
+        edit_menu.addAction("Copy")
+        edit_menu.addAction("Copy")
         
         button=QPushButton("Click Me!")
         self.setCentralWidget(button)
